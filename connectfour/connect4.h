d0209b0ec6bd2952641d5259d1c0d95f51b44299
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2018-11-06 17:40:38 +0800

    comments

--- connectfour/connect4.h
+++ connectfour/connect4.h
@@ -7,14 +7,14 @@ using namespace std;
 class connect4
 {
 public:
-	connect4();
-	connect4(char board[boardy][boardx],char turn);
-	void move(int i);
-	bool validmove(int i);
+	connect4();//empty board
+	connect4(char board[boardy][boardx],char turn);//board is a copy
+	void move(int i);//attept move
+	bool validmove(int i);//check if valid
 	
-	string tostring();
-	char winner();
-	char turn();
+	string tostring();//for printing
+	char winner();//return winning side s for stalemate '#' for no one
+	char turn();//retunrs turn
 
 private:
 	char player;
